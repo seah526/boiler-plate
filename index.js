5ff44b0c8c9386ef1823aa758073a2b661df6f6c
seah526 <seah526@naver.com>
2022-06-13 16:17:53 +0900

    ignore private data

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://seah:dlatpdk526@boiler-plate.b6y5ekj.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err)) 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하세요')
+})
+
+app.post('/register', (req, res) => {
+    //회원가입에 필요한 정보를 client에서 받아와 DB에 넣어준다
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({success:false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
 })
 
 app.listen(port, () => {
